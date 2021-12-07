6eaa3ff4a177ae6c9d14620620ed9cdaf745392b
Wang-qing-yuan <1950016253@qq.com>
2021-12-07 16:40:26 +0800

    跨域问题

---
+++ src/main/java/top/wqy/myblog/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package top.wqy.myblog.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @description: 跨域配置类
+ * @author: mqxu
+ * @date: 2021-12-07
+ **/
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
